8f2147aaf0bbcae5c452716b672f784fdcab3aa0
msaad973 <muhammadsaadullah973@gmail.com>
2025-01-15 18:23:30 +0500

    change color and text center

--- advanced/src/App.js
+++ advanced/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h3>Advanced React</h3>
+      <h3 style={{ color: 'red',textAlign: 'center' }}>Advanced React</h3>
     </div>
   );
 }
