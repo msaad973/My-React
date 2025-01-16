da59efeca4cbdd307325cbe54afceb0ca807d2c3
msaad973 <muhammadsaadullah973@gmail.com>
2025-01-16 11:47:23 +0500

    delete dive and import use state component

---
+++ advanced/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const[count, setCount] = useState(0)
+  return (
+    
+      <h3 style={{ color: 'red',textAlign: 'center' }}>Advanced React</h3>
+    
+  );
+}
+
+export default App;
