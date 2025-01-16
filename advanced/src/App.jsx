27380a3c64ff76c1213a47e81130283f56b24868
msaad973 <muhammadsaadullah973@gmail.com>
2025-01-16 11:52:15 +0500

    add a div and counter button

--- advanced/src/App.jsx
+++ advanced/src/App.jsx
@@ -3,12 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const[count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
   return (
-    
-      <h3 style={{ color: 'red',textAlign: 'center' }}>Advanced React</h3>
-    
+    <>
+      <h3 style={{ color: 'red', textAlign: 'center' }}>Advanced React</h3>
+      <div className='card'>
+        <button onClick={() => setCount(prevCount => prevCount + 1)} >count is {count}</button>
+
+      </div>
+
+
+    </>
   );
 }
-
 export default App;
