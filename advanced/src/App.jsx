7b14d5154e3c6fe91beedbfe9479d28e76456a0a
msaad973 <muhammadsaadullah973@gmail.com>
2025-01-23 12:31:40 +0500

    add increment buttton and 2nd use Effect and alert

--- advanced/src/App.jsx
+++ advanced/src/App.jsx
@@ -3,15 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(0);
   useEffect(() => {
-    alert('you clicked me');
-  })
+    alert('You have increment the value.');
+  }, [increment]);
+  
+  useEffect(() => {
+    alert('You have Count the value.');
+  }, [count]);
   return (
     <>
-     <div>The Count is {count}</div>
-     <button onClick={()=>{setCount(count+2)}}>Update Count</button>
+      <div>The Count is {count}</div>
+      <button onClick={() => { setCount(count + 2) }}>Update Count</button>
 
+      <div>The Increment is {increment}</div>
+      <button onClick={() => { setIncrement(increment + 1) }}>Update Value</button>
     </>
   );
 }
